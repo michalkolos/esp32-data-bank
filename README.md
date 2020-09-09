105bb89c717ac2792c513f5ce10d50e028eb3524
Michalkolos <cool.kolos@gmail.com>
2020-09-09 12:48:56 +0200

    Readme and documentation.

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
 # esp32-data-bank
+
+ESP32 microcontroller allows to store data during sleep state in its RTC memory. 
+However only statically allocated primitive or c-struct objects are allowed.
+
+This implementation of cicular buffer allows to store any continous data 
+including c++ class objects when ESP32 is in sleep mode and its main memory is 
+powered off.
+
+
+## Usage
+
+### 1. Memory allocation:
+
+``` C
+RTC_DATA_ATTR uint8_t bufferMemory[MEMORY_SIZE];
+```
+
+Allocated memory does not need to be in the RTC_DATA space. However then all 
+storred data will be lost on wake-up.
+
+### 2. Declaring DataBank object:
+
+``` C
+DataBank<FooClass> dataBank(bufferMemory, MEMORY_SIZE);
+```
+
+DataBank is a generic type FooClass represents a class of objects to store in the buffer.
+
+BufferMemory is a pointer to the beginning of the allocated space.
+
+### 3. Initializing buffer:
+
+``` C
+dataBank.init();
+```
+
+Clears the memory and sets internal indexes to the beginning. Can be called multiple times to reset the DataBank.
+
+### 4. Operations:
+
+* Putting objects on the buffer:
+
+``` C
+dataBank.push(obj);
+```
+
+This creates a copy of the object in the buffer memory. If the memory allocated for the bank is exceeded, the oldest stored object gets overwritten.
+
+* Getting objects from the buffer:
+  
+``` C
+dataBank.pop(&obj, true);
+```
+
+Returns the oldest stored element. Data from the buffer will be copied to the object under given address. Second parameter determines if the source data in the buffer gets deleted.
+
+## TODO
+
+* Implement random access to the data.
+* Implement accessing the data without copying memory contents.
--- include/DataBank.h
+++ include/DataBank.h
@@ -1,3 +1,15 @@
+/**
+ * @file DataBank.h
+ * @author Michal Kolosowski (you@domain.com)
+ * @brief Generic circular buffer implementation that stores data in a staticly 
+ * allocated memory.
+ * @version 0.1
+ * @date 2020-09-09
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
 #ifndef DATA_BANK_H
 #define DATA_BANK_H
 
@@ -9,6 +21,9 @@ class DataBank
 {
 private:
 
+    /**
+     * Buffer state data placed at the beginning of the reserved memory.
+     */
     struct BufferState
     {
         uint16_t readIndex;
@@ -16,22 +31,41 @@ private:
         bool empty;    
     };
 
-
+    
+    /// Pointer to the beginning of reserved memory.
     void* startAddress;
+
+    /// Bytesize of a single storred object .
     size_t dataLen;
+    
+    /// Bytesize of the total allocated memory.
     size_t memoryLen; 
+
+    /// Maximum number of objects allowed on the buffer.
     uint16_t memorySize;
 
+    /// Pointer to the object storred in the allocated memory that holds current
+    /// state of the buffer
     BufferState* bufferState;
 
-
+    /**
+     * @brief Moves a given index one position forwards. At the end returns it
+     * to the beginning of the range.
+     * 
+     * @param startingPos Pointer to the index.
+     */
     void advanceIndex(uint16_t* startingPos){
         *startingPos = ++(*(uint8_t*)startingPos) % memorySize ;
 
         return;
     }
 
-    
+    /**
+     * @brief Converts index position to memory address it represents.
+     * 
+     * @param pos Index.
+     * @return void* Data address in buffer memory.
+     */
     void* resolveMemOffset(uint16_t pos){
         return (uint8_t*)startAddress + (dataLen * pos);
     }
@@ -41,6 +75,13 @@ private:
 
 
 public:
+
+    /**
+     * @brief Construct a new DataBank object
+     * 
+     * @param startAddress Pointer to the beginning of the memory for the buffer.
+     * @param memoryLen Length of the memory for the buffer
+     */
     DataBank(void* startAddress, size_t memoryLen){
         
         dataLen = sizeof(T);
@@ -50,25 +91,29 @@ public:
         bufferState = (BufferState*)startAddress;
         this->startAddress = (uint8_t*)startAddress + sizeof(BufferState);
 
-
     return;
     }
     
     
-    
     ~DataBank(){
-
     }
 
 
-
-
+    /**
+     * @brief Clears the memory and resets all indexes.
+     * 
+     */
     void init(){
         memset((uint8_t*)startAddress - sizeof(BufferState), 0, memoryLen);
         this->bufferState->empty = true;    
     }
 
 
+    /**
+     * @brief Returns a number of storrend objects in the buffer.
+     * 
+     * @return uint16_t Number of objects.
+     */
     uint16_t use(){
         if(bufferState->empty){ return 0; }
 
@@ -83,7 +128,11 @@ public:
         return count;
     }
 
-
+    /**
+     * @brief Puts a copy of given object into the buffer.
+     * 
+     * @param inData Reference to the source object 
+     */
     void push(T& inData){
          if(memorySize <= 0){ return; }
 
@@ -102,7 +151,15 @@ public:
 
 
 
-
+    /**
+     * @brief Gets oldest element storred in the buffer. 
+     * 
+     * @param outData Address of an object that will be overwritten by data 
+     * from the buffer.
+     * @param move Flag that decides whether returning object is moved or copied 
+     * form the buffer.
+     * @return uint16_t Number of objects left in the buffer.
+     */
     uint16_t pop(T* outData, bool move){
                 
         if(bufferState->empty){ 
@@ -124,12 +181,7 @@ public:
     }
 
 
-
-
-    void dump(void* outData, int buffSize, bool move); 
-    int getUsage();
-    int getMaxUsage();
-
+    
     String toString(){
         String outString = "MemLen: ";
         outString += memoryLen;
